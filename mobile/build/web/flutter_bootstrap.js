b765baf38029ed3ca6167a1a88f1113b391f6db2
mnllmnd <mnllmnd@outlook.fr>
2025-12-18 09:37:24 +0000

    llmd

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "422235523"
+    serviceWorkerVersion: "588634784"
   }
 });
